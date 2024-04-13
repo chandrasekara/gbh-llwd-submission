65aa56a00903b2395db2c2d1a36d176c0c490ed1
Your Name <you@example.com>
2024-04-13 14:02:37 +1000

    submission v4: augmented moving average policy3

--- bot/policies/augmented_moving_average.py
+++ bot/policies/augmented_moving_average.py
@@ -6,7 +6,7 @@ from policies.policy import Policy
 # out of the box provided by hackathon - uses moving average to decide on actions
 
 class AugmentedMovingAveragePolicy(Policy):
-    def __init__(self, window_size=5):
+    def __init__(self, window_size=100):
         """
         Constructor for the MovingAveragePolicy.
 
