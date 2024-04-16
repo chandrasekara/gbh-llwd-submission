a535e159754fa7bc326222f2d4120e0c60513355
Your Name <you@example.com>
2024-04-16 18:12:42 +1000

    submission v31: dont export if the price is increasing but still low

--- bot/policies/augmented_moving_average.py
+++ bot/policies/augmented_moving_average.py
@@ -16,13 +16,19 @@ class AugmentedMovingAveragePolicy(Policy):
         self.window_size = window_size
         self.price_history = deque(maxlen=window_size)
         self.battery_capacity_kwh = 13
+        self.observed_maximum_price = 1
 
     def act(self, external_state, internal_state):
 
         solar_to_battery = 0
 
         market_price = external_state['price']
+        if market_price > self.observed_maximum_price and market_price < 300:
+            self.observed_maximum_price = market_price
+        
         self.price_history.append(market_price)
+
+        #print(self.observed_maximum_price)
 
         if float(market_price) < 0:
             charge_kW = internal_state['max_charge_rate']
@@ -31,13 +37,16 @@ class AugmentedMovingAveragePolicy(Policy):
             moving_average = np.mean(self.price_history)
             
             if market_price > moving_average:
-                charge_kW = -internal_state['max_charge_rate']
+                if market_price > 0.5 * self.observed_maximum_price:
+                    charge_kW = -internal_state['max_charge_rate']
+                else:
+                    charge_kW = 0.1 * -internal_state['max_charge_rate']
                 ## LOW_BATT_THRESHOLD should be very small, around 20% at absolute maximum
                 ## CHARGE_SCALE_FACTOR should take into account not being too high to avoid the sales to the grid but not too low such
                 ## that we get periods of 0% capacity where we'll miss out on sales when the price is high due to battery being drained
                 ## and pv == 0
-                if (float(internal_state["battery_soc"])/13) < 0.2:
-                    solar_to_battery = int(0.2 * int(float(external_state['pv_power'])))
+                #if (float(internal_state["battery_soc"])/13) < 0.2:
+                #    solar_to_battery = int(0.2 * int(float(external_state['pv_power'])))
 
                 #if battery_capacity < LOW_BATT_THRESHOLD:
                 #   solar_to_battery = int(CHARGE_SCALE_FACTOR * int(float(external_state['pv_power'])))
